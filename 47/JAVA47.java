4161fef97c44e91ea54c29efa6c8578ad3ce0a4e
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2020-09-16 11:48:31 +0530

    Create JAVA47.java

---
+++ 47/JAVA47.java
@@ -0,0 +1,26 @@
+class JAVA47
+{
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=6;i++)
+        {
+            System.out.print("*"+" ");
+        }
+        System.out.println();
+        for(int a=1;a<=4;a++)
+        {
+            System.out.print("*");
+            for(int b=1;b<=9;b++)
+            {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            System.out.println();
+        }
+        for(int j=1;j<=6;j++)
+        {
+            System.out.print("*"+" ");
+        }
+        
+    }
+}
