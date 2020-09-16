de3325a8982a56363ee8cf1d27e10c99c2cb6227
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2020-09-16 11:48:43 +0530

    Create JAVA52.java

---
+++ 52/JAVA52.java
@@ -0,0 +1,29 @@
+class JAVA52
+{
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=7;i++)
+        {
+            for(int j=1;j<=7;j++)
+            {
+                if((i==1||i==7)||((j==1)||(j==7)))
+                {
+                    System.out.print("* ");
+                }
+                else if(((i==3)||(i==5))&&((j==3)||(j==4)||(j==5)))
+                {
+                    System.out.print("* ");
+                }
+                else if((i==4)&&((j==3)||(j==5)))
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
