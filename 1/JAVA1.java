b7dad136454424f7e35cfc97ec17a6864d797340
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2020-09-16 10:49:39 +0530

    Create JAVA1.java

---
+++ 1/JAVA1.java
@@ -0,0 +1,12 @@
+public class JAVA1
+{
+    public static void main(String[] args)
+    {
+        int a=1;
+        for(int i=2;i<=5+1;i++)
+        {
+            System.out.println(a+" ");
+            a=a*10+i;
+        }
+    }
+}
