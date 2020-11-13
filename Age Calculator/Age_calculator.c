67a8c3202a8e26b8a5d61619b20d32d920aec8d5
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2020-11-13 22:02:13 +0530

    age

--- Age Calculator/Age_calculator.c
+++ Age Calculator/Age_calculator.c
@@ -16,7 +16,8 @@ void age(int p_d, int p_m, int p_y, int b_d, int b_m, int b_y) {
    int f_y = p_y - b_y;
    printf("Present Age Years: %d Months: %d Days: %d", f_y, f_m, f_d);
 }
-int main() {
+
+void main() {
    int p_d,p_m,p_y , b_d , b_m , b_y ;
    printf("Enter present date : ");
    scanf("%d",&p_d);
@@ -33,5 +34,4 @@ int main() {
    printf("Present date : %d.%d.%d\n",p_d,p_m,p_y);
    printf("Birth date : %d.%d.%d\n",b_d,b_m,b_y);
    age(p_d, p_m, p_y, b_d, b_m, b_y);
-   return 0;
 }
