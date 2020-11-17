86d82de8beaa0ba3f5abb9a384dd3ab4773f9baf
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2020-11-17 20:35:15 +0530

    Armstrong Number

--- Number Problems/Armstrong Number/Armstrong_No.cpp
+++ Number Problems/Armstrong Number/Armstrong_No.cpp
@@ -1,27 +1,35 @@
 #include<iostream>
 using namespace std;
-void main(){
-	int n,n1,d,a,s=0;
-	cout<<"Enter a number: ";
-	cin>>n;
-	/*n1=n;
-	while(n1!=0){
-		d++;
-		n1=n1/10;
-	}*/
-	n1=n;
-	while(n1!=0){
-		a=n1%10; //getting each digit
-	//	if(d==3)
-			s=s+(a*a*a);//summation
-	//	if(d==2)
-	//		s=s+(a*a);
-		n1=n1/10;
-	}
-	if(s==n){
-		cout<<"ArmStrong number";
-	}
-	else{
-		cout<<"Not Armstrong number";
-	}
+int num_digits(int n){
+    int nd=0;
+    for(nd=0;n!=0;nd++,n=n/10);
+    return nd;
+}
+
+int powe(int d,int p){
+    int po=1,j;
+    for(j=1;j<=p;j++)
+        po=po*d;
+    return po;
+}
+
+int armstrong(int n){
+    int r,n2,t=num_digits(n),s=0,y,j;
+    for(n2=n;n2!=0;n2/=10)
+        s=s+powe(n2%10,t);
+    if(n==s)
+        return 1;
+    else
+        return 0;
+}
+
+int main(){
+    int n,i;
+    cout<<"Enter A Number : ";
+    cin>>n;
+    if(armstrong(n)==1)
+        cout<<n<<" is an Armstrong Numeber";
+    else
+        cout<<n<<" is not a Armstrong Number";
+    return 0;
 }
--- Number Problems/Armstrong Number/Armstrong_No.java
+++ Number Problems/Armstrong Number/Armstrong_No.java
@@ -1,35 +1,34 @@
 import java.util.*;
-class ArmstrongNo
+class Armstrong_No
 {
-    public boolean arm(int n)
-    {
-        int r=0;
-        int o=n;
-        while(n>0)
-        {
-            int d=n%10;
-            r=r+(d*d*d);
-            n=n/10;
-        }
-        if(o==r)
-        {
+    int num_digits(int n){
+        int nd=0;
+        for(nd=0;n!=0;nd++,n=n/10);
+        return nd;
+    }
+    boolean arm(int n){
+        int n2,t=num_digits(n),s=0;
+        for(n2=n;n2!=0;n2/=10)
+            s=s+(int)Math.pow(n2%10,t);
+        if(n==s)
             return true;
-        }
         return false;
     }
     
-    public void main()
+    public static void main(String[] args)
     {
         Scanner sc=new Scanner (System.in);
-        System.out.println("Enter a number");
+        System.out.print("Enter a number : ");
         int m=sc.nextInt();
-        if(arm(m)==true)
+        sc.close();
+        Armstrong_No ob=new Armstrong_No();
+        if(ob.arm(m)==true)
         {
-            System.out.println("Armstrong");
+            System.out.print(m+" is an Armstrong number");
         }
         else
         {
-            System.out.println("Not Armstrong");
+            System.out.print(m+" is not an Armstrong number");
         }
     }
 }
