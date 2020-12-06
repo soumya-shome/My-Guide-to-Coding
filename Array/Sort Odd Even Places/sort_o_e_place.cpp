e01eecd18ed019e834011e7aafcfbd75435001df
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2020-12-06 23:39:58 +0530

    All Folders set

---
+++ Array/Sort Odd Even Places/sort_o_e_place.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+ void funcswap(int [],int);
+ 
+ void funcswap(int a[],int s){
+ 	for(int i=0;i<s;i+=2){
+ 		int t=i+1;
+ 		int y=a[i];
+ 		a[i]=a[t];
+ 		a[t]=y;
+	}
+ }
+  
+main(){
+	int n;
+	cout<<"Enter the Number of terms : ";
+	cin>>n;
+	int b[n];
+	cout<<"Enter "<<n<<" numbers : ";
+	for(int i=0;i<n;i++)
+		cin>>b[i];
+	funcswap(b,n);
+	cout<<"the Swapped array : ";
+	for(int i=0;i<6	;i++)
+		cout<<b[i]<<",";
+}
