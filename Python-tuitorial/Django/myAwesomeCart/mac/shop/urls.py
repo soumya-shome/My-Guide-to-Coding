fd1112995880f05ea8efad78ec4f2591d2ba8dc7
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2025-02-02 18:01:43 +0530

    02-02-25

--- Python-tuitorial/Django/myAwesomeCart/mac/shop/urls.py
+++ Python-tuitorial/Django/myAwesomeCart/mac/shop/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index, name = "ShopHome")
+    path("", views.index, name="ShopHome"),
+    path('contact/', views.contact,name="ContactUs"),
+    path('tracker', views.tracker,name="TrackingStatus"),
+    path('search/', views.search,name="Search"),
+    path('productview/', views.productView,name="TrackingStatus"),
+    path('checkout/', views.checkout,name="Checkout"),
 ]
