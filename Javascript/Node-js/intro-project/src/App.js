1c3152cd4c6bc44c43b0d7a29e224670112099f7
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2026-05-03 23:55:31 +0530

    a

---
+++ Javascript/Node-js/intro-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my first App</h1>
+      <p>Hellooo!!!</p>      
+    </div>
+  );
+}
+
+export default App;
